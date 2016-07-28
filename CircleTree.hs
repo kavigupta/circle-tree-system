e5382ac602c3fbb3b6c3ae916a6da4da370070b7
Kavi Gupta <kavi@globalmbox.com>
2016-07-27 20:54:23 -0700

    Rename 32236 to ?

--- CircleTree.hs
+++ CircleTree.hs
@@ -3,7 +3,8 @@ import CircleTreeCalculus
 import CircleTreeOutput
 
 main :: IO ()
-main = writeToFile "y-combinator.svg" 400 $ Lambda X (Lambda Y (x $$ (y $$ y)) $$ Lambda Y (x $$ (y $$ y)))
+main = writeToFile "eg/y-combinator.svg" 400 $ Lambda X (value ? value)
+    where value = Lambda Y (x ? (y ? y))
 
 -- identity :: LCalc
 -- identity = Lambda "x" x
--- CircleTreeCalculus.hs
+++ CircleTreeCalculus.hs
@@ -1,17 +1,21 @@
 module CircleTreeCalculus(
         LCalc(..),
         Variable(..),
-        ($$),
-        x, y, z, w, t, a, b, c
+        (?),
+        x, y, z, w, t, a, b, c,
+        lambda
     ) where
 
 data Variable = X | Y | Z | W | T | A | B | C deriving (Show)
 
 data LCalc = Var Variable | Lambda Variable LCalc | App LCalc LCalc deriving (Show)
 
-infixl 9 $$
-($$) :: LCalc -> LCalc -> LCalc
-($$) = App
+lambda :: [Variable] -> LCalc -> LCalc
+lambda = foldr ((.) . Lambda) id
+
+infixl 9 ?
+(?) :: LCalc -> LCalc -> LCalc
+(?) = App
 
 x, y, z, w, t, a, b, c :: LCalc
 [x, y, z, w, t, a, b, c] = map Var [X, Y, Z, W, T, A, B, C]
