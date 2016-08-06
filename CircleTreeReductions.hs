e0cfe526e0b512eb06db50608e81c58e2c2a6721
Kavi Gupta <kavi@globalmbox.com>
2016-08-06 13:57:48 -0700

    Added concept of eta reduction

--- CircleTreeReductions.hs
+++ CircleTreeReductions.hs
@@ -22,14 +22,20 @@ lambdaPos s = p2 (xPos s, 0)
 
 infixr 5 :--:
 infixr 5 :->:
-data Reduction = LCalc :--: Reduction | LCalc :->: Reduction | Final LCalc
+infixr 5 :-<:
+data Reduction = LCalc :--: Reduction | LCalc :->: Reduction | LCalc :-<: Reduction | Done
 
 drawReduction :: Reduction -> Diagram B
 drawReduction = center . renderObjects' FirstStep
     where
-    renderObjects' pos (Final term) = position [(lambdaPos pos, drawLambda term # named pos # center)]
+    renderObjects' _ Done = error "Invalid Reduction object"
+    renderObjects' pos (term :->: Done) = displayLast pos term
+    renderObjects' pos (term :--: Done) = displayLast pos term
+    renderObjects' pos (term :-<: Done) = displayLast pos term
     renderObjects' pos (term :->: rest) = twoSegs pos "Burst" term rest
     renderObjects' pos (term :--: rest) = twoSegs pos "Recolor" term rest
+    renderObjects' pos (term :-<: rest) = twoSegs pos "Create" term rest
+    displayLast pos term = position [(lambdaPos pos, drawLambda term # named pos # center)]
     twoSegs pos label term rest
             = position [(lambdaPos pos, lambImage), (textPos, textLabel)]
                 <> renderObjects' (NextStep pos) rest
